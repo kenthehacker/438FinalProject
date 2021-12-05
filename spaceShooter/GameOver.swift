0cf1fe1bb7bf1bd35723830d72b1a05229845172
ertseng2000 <54486489+ertseng2000@users.noreply.github.com>
2021-12-05 17:24:45 -0600

    wip

--- spaceShooter/GameOver.swift
+++ spaceShooter/GameOver.swift
@@ -36,17 +36,6 @@ class GameOver: UIViewController {
         navigationController?.pushViewController(leaderboardVC, animated: true)
     }
     
-    @IBAction func play_button(_ sender: UIButton) {
-        let playVC = storyboard!.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-        playVC.createEnemyFormation()
-        playVC.backgroundMusic()
-        playVC.gameView.mainCharacter.health = 200
-        playVC.gameView.numEnemy = 0
-        gameClock = CADisplayLink(target: self, selector: #selector(playVC.update))
-        gameClock?.add(to: .current, forMode: .common)
-        navigationController?.pushViewController(playVC, animated: true)
-    }
-    
     /// Returns user to home
     @IBAction func rth_button(_ sender: UIButton) {
         let launchVC = storyboard!.instantiateViewController(withIdentifier: "LaunchViewController") as! LaunchViewController
