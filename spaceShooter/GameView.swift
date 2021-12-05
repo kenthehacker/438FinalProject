ddb9798c18ef6011e265b22e2583737ccf999945
Kenichi Matsuo <kenichimatsuo@kenichis-mbp.dhcp.wustl.edu>
2021-12-05 16:14:01 -0600

    asdf

--- spaceShooter/GameView.swift
+++ spaceShooter/GameView.swift
@@ -87,7 +87,6 @@ final class GameView: UIView{
     }
     func scaledCheckKillEnemy(loc: CGPoint){
         var i = 0
-        print(loc)
         while i < scaledEnemies.count{
             if scaledEnemies[i].contains(point: loc){
                 scaledEnemies.remove(at: i)
